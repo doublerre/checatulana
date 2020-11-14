4e886645e8071f45e5691ff32e6c02acf492699c
Jonathan <honjoni16@gmail.com>
2020-11-14 15:22:54 -0600

    front dash refine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
           <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="/login">Login</a>
+        <a class="nav-link" href="/login">Iniciar Sesion</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/register">Registro</a>
